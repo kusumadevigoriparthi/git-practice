dd1e83aef46ffe4b85bd2be7d83e76227e8853b5
kusumadevigoriparthi <kusumadevigoriparthi@gmail.com>
2026-09-23 12:19:11 +0530

    Practice git pull

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main(void)
     printf("Git practice - Embedded C\n");
     printf("Learning Git and GitHub\n");
     printf("LED feature development\n");
+    printf("Git pull practice\n");
    
     return 0;
 }
